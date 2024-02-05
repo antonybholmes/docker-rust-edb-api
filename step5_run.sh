68d71ad83a590aff6e03d75f74721e11c971b351
Antony Holmes <antony@antonyholmes.dev>
2024-02-05 12:13:58 -0500

    fix: Bug fixes and updates.

--- step5_run.sh
+++ step5_run.sh
@@ -2,5 +2,5 @@
 #docker-compose down
 #docker image rm docker-rust-edb-api-web
 #docker-compose up -d
-./step3_rebuild_nginx.sh
-./step4_rebuild_edb-api.sh
+./step3_rebuild_edb-api.sh
+./step4_rebuild_nginx.sh
