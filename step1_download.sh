b848c04dcf80862bb7604a160159cf083e17f448
Antony Holmes <antony@antonybholmes.com>
2024-02-18 21:24:39 -0500

    fix: Bug fixes and updates.

--- step1_download.sh
+++ step1_download.sh
@@ -1,6 +1,8 @@
 git clone git@github.com:antonybholmes/rust-edb-api.git
 git clone git@github.com:antonybholmes/rust-dna.git
-git clone git@github.com:antonybholmes/rust-loctogene.git
+git clone git@github.com:antonybholmes/rust-auth.git
+git clone git@github.com:antonybholmes/rust-sys.git
+git clone git@github.com:antonybholmes/rust-genes.git
 
 pwd=`pwd`
 for d in `find . -maxdepth 1 -type d`
@@ -9,3 +11,6 @@ do
 	git pull
 	cd ${pwd}
 done
+
+mkdir -p data
+mkdir -p logs
